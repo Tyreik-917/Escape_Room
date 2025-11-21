a71b95faf0261e33b76d7c9fdd533e89b3772351
gavrielguenniche11-arch <gavrielguenniche11@gmail.com>
2025-11-21 00:16:55 -0500

    Enhance Item class with interaction messages
    Refactor Item class to include message callback and glow logic.

--- item.py
+++ item.py
@@ -1,5 +1,6 @@
 # item.py
 import pygame
+import time
 
 screen_w = 1920
 screen_h = 1080
@@ -14,18 +15,20 @@ class Item:
         self.image = pygame.image.load(image_path)
         world_x, world_y = pos
         screen_x = world_x + half_w
-        screen_y = world_y + half_h
+        screen_y = half_h - world_y
         self.rect = self.image.get_rect(center=(screen_x, screen_y)) #centers image starting pos
         self.collision = collision
         self.interactable = interactable
-        
+        self.near_start = None
+        self.glow = False
+
         resize = (self.rect.width*size[0], self.rect.height*size[1])
         if resize != None:
             self.image = pygame.transform.scale(self.image, resize)
         self.rect = self.image.get_rect(center=(screen_x, screen_y))
         world_x = max(-half_w + self.rect.width//2, min(world_x, half_w - self.rect.width//2))
         world_y = max(-half_h + self.rect.height//2, min(world_y, half_h - self.rect.height//2))
-        self.rect.center = (world_x + half_w, world_y + half_h)
+        self.rect.center = (world_x + half_w, half_h - world_y)
 
         x = max(self.rect.width, 150)
         y = max(self.rect.height, 150)
@@ -45,12 +48,21 @@ class Item:
         distance = (dx,dy)
         return distance[0] < self.interact_radius[0] and distance[1] < self.interact_radius[1]
 
+    def set_message_callback(self, callback):
+        self.show_message = callback
+
     def draw(self, surface, player_rect, show_hitbox=False):
         if not self.is_active:
             return
         surface.blit(self.image, self.rect.topleft)
-        if self.is_near(player_rect) and self.interactable:
+        if self.is_near(player_rect) and self.interactable and self.glow:
             surface.blit(self.glow_surface, self.rect.topleft)
+            if self.near_start is None:
+                self.near_start = time.time()
+            if time.time() - self.near_start >= 10:
+                self.show_message(f"You can interact with {self.name} (press E)")
+        else:
+            self.near_start = None
         if show_hitbox:
             pygame.draw.rect(surface, (0, 255, 0), self.rect, 2)
 
@@ -59,22 +71,36 @@ class Item:
 
 
 #Specialized item types
-
-class Crate(Item):
+# level 1
+class Statue_m(Item):
     def interact(self):
-        print("crate")
+        wine = globals().get('wine', False)
+        if wine == False:
+            self.show_message(f"The statue seems to be missing something...", 3)
+        elif wine == True:
+            self.show_message(f"You gave the wine to the statue. It seems satisfied.", 3)
         self.is_finished = True
 
-class Table(Item):
+class Statue_f(Item):
     def interact(self):
-        print("table")
-        self.is_finished = True
-
-class Statue(Item):
-    def interact(self):
+        global wine
+        wine = True
         print("statue")
         self.is_finished = True
 
+class Picture(Item):
+    def interact(self):
+        print("picture")
+        self.is_finished = True
+
+
+# level 2
+
+
+# level 3
+
+
+# level 4
 
 
 #same for each level
