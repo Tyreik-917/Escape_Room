7ec520be649e13f719425e6bd5bc83aee9a0a774
tyreik <tyreikr11@gmail.com>
2025-12-05 03:30:51 -0500

    Changes

--- level.py
+++ level.py
@@ -1,40 +1,148 @@
 import pygame
-from item import Item, Statue_m, Statue_f, Picture, Door
+from item import Item, Carpet, Statue_m, Statue_f, Picture, Door, Chest, Shovel, Knife, Trash, MusicBox, Bookshelf, Dresser, Microwave, Nightlight, Ladder, SmokeDetector, Color, Hammer, Hole, Red_Mouse, Grey_Mouse, Cheese_man, Barrel_1, Barrel_2, Barrel_3, Vent_6, Vent_7, Code_box, Power_Bank, Barrier_1, Barrier_2, Barrier_3, Barrier_4, Barrier_5
+import os
+import platform
+import ctypes
 
-width, height = 1920//2, 1080//2
-center_x, center_y = 0,0
+if platform.system() == "Windows":
+    try:
+        ctypes.windll.user32.SetProcessDPIAware()
+    except Exception:
+        pass
+
+if platform.system() == "Darwin":
+    os.environ["SDL_HINT_VIDEO_HIGHDPI_DISABLED"] = "0"
+    os.environ["PYGAME_FORCE_HIGHDPI"] = "1"
+
+pygame.init()
+width, height = 1920, 1080
+center_x, center_y = width // 2, height // 2
+
 class Level:
     def __init__(self, level_id, show_message_callback):
         self.level_id = level_id
         self.show_message = show_message_callback
         self.items = []
+        width, height = center_x, center_y
         # Load items depending on the level
         if level_id == 1:
             self.items = [
-                Door("door", "table.png", (0, 0), (1,1),False, False),
-                Picture("picture", "picture.png", (0, height)),
-                Statue_m("statue", "statue_m.png", (-200, height)),
-                Statue_f("statue", "statue_f.png", (200, height)),
+                Door("trapdoor", "Assets/trapdoor.png", (0, -100), (1.5,1.5), False, False),
+                Carpet("carpet", "Assets/carpet.png", (0, -100), (1,1), False),
+                Picture("picture", "Assets/picture.png", (0, height), (1,1), False, False),
+                Statue_m("statue", "Assets/statue_m.png", (-200, height), (1,1), False, False),
+                Statue_f("statue", "Assets/statue_f.png", (200, height), (1,1), False, False),
+                Knife("knife", "Assets/knife.png", (-width+140, height-110), (1,1), False, False, never_interactable=True),
+                Shovel("shovel", "Assets/shovel.png", (width-140, height-115), (1,1.5), False, False),
+                Trash("trash", "Assets/trash.png", (-width, -330), (1,1), True, False),
+                Chest("chest", "Assets/chest.png", (-width, -250)),
+                MusicBox("music box", "Assets/music_box.png", (-width//2 + 50, height-200), (1,1), False, False),
+                Bookshelf("bookshelf", "Assets/bookshelf.png", (width, -height+375), (1,1), True, False),
             ]
-            #name,address, (pos) (0,0) = center, (size) * scale, collision (naturally on), interactable (naturally on)
-            self.background = pygame.image.load("background.png")
+            Knife.is_finished = True
+            self.background = pygame.image.load("Assets/attic.png").convert()
+            self.show_message("Oh no, I've been kidnapped I must ESCAPE", size = 32, queue=True)
+            self.show_message("I can use 'W','A','S','D' to get around", size = 32, queue=True)
+            self.show_message("I can use 'E' to interact with items that glow", size = 32, queue=True)
+
         elif level_id == 2:
             self.items = [
-                Door("door", "table.png", (0, 0), (1,1),False, False),
-            ]  
-            self.background = pygame.image.load("background.png")
+                Door("bedroom door", "Assets/bedroom_door.png", (0, 345), (1,1), False, False),
+                Dresser("dresser", "Assets/dresser.png", (-400, 300), (.66,.66), True, True),
+                Microwave("microwave", "Assets/microwave.png", (250, 270), (.66,.66), True, True),
+                Nightlight("nightlight", "Assets/nightlight.png", (-width+30, height-300), (1,1), False, True),
+                Ladder("ladder", "Assets/ladder.png", (-250, 300), (.66,1), True, True),
+                SmokeDetector("smoke detector", "Assets/smoke_detector.png", (400, 370), (1,1), False, True), 
+                Color("color code", "Assets/color.png", (30, 325), (1,1), False, True),
+            ]
+            self.background = pygame.image.load("Assets/bedroom.png").convert()
+            self.background = pygame.transform.scale(self.background, (width*2, height*2))
+            self.show_message("I made it downstairs, but the door is locked!", size = 32, queue=True)
+            self.show_message("It looks like I need a code for the door", size = 32, queue=True)
+
         elif level_id == 3:
-            self.items = [
-                Door("door", "table.png", (0, 0), (1,1),False, False),
-            ]  
-            self.background = pygame.image.load("background.png")
+            self.items = [ 
+                          Door("pit","Assets/exit.png",(-600,-360),(1,1),False,True),
+                          Hammer("chest","Assets/chest.png",(0,250), (1,1), True, True),
+                          Hole("hole","Assets/hole.png",(-width+150,height-300), (1,1), False, True),
+                         ]
+            self.background = pygame.image.load("Assets/basement.png").convert()
+            self.show_message("It looks like I am in a basement now", size=32, queue=True)
+            self.show_message("I wonder what the dark pit by the bottom leads to", size=32, queue=True)
+
         elif level_id == 4:
             self.items = [
-                Door("door", "table.png", (0, 0), (1,1),False, False),
-            ]  
-            self.background = pygame.image.load("background.png")
+                Door("door", "Assets/exit_door.png", (-155 , height - 160),
+                size=(.25, .25), collision=False, interactable=True),
+                
+                Vent_6("vent", "level_4/vent.png", (85 , height - 450),
+                size=(1.04, .88), collision=False, interactable=True),
+                
+                Vent_7("vent", "level_4/vent.png", (-40 , - height),
+                size=(1.04, 0.88), collision=False, interactable=True),
+                
+                Vent_6("vent", "level_4/vent.png", (-780 , height - 310),
+                size=(1.04, 0.88), collision=False, interactable=True),
+                
+                Vent_7("vent", "level_4/vent.png", (580 , height - 570),
+                size=(1.04, 0.88), collision=False, interactable=True),
+                
+                Code_box("code_box", "level_4/Pipe_line.png", (-275 , height - 210),
+                size=(0.13, 0.11), collision=False, interactable=True),
+                
+                Power_Bank("power_bank", "level_4/power_bank.png", (660 , height - 195),
+                size=(0.12, 0.1), collision=False, interactable=True),
+                
+                Cheese_man("cheese_toy", "level_4/Mr_Cheese.png.gif", (70 , - height),
+                size=(0.13, 0.11), collision=False, interactable=True),
+                
+                Grey_Mouse("grey_mouse", "level_4/Grey_Mouse.png", (85, height - 220),
+                size=(0.169, 0.143), collision=False, interactable=True),
+                
+                Red_Mouse("red_mouse", "level_4/Red_Mouse.png", (455, height - 220),
+                size=(0.169, 0.143), collision=False, interactable=True),
+                
+                Barrel_2("barrel", "level_4/Barrel.png", (780 , height - 525),
+                size=(1.1, 1), collision=False, interactable=True),
+                
+                Barrel_1("barrel", "level_4/Barrel.png", (-width , height - 300),
+                size=(1.1, 1), collision=False, interactable=True),
+                
+                Barrel_3("barrel", "level_4/Barrel.png", (-170 , -height + 145),
+                size=(1.1, 1), collision=False, interactable=True),
+
+                Barrier_1("barrier", "Assets/exit.png", (-width , -height),
+                size=(2.8, 2.8), collision=True, interactable=False, never_interactable=True),
+
+                Barrier_2("barrier", "Assets/exit.png", (width , -height),
+                size=(4, 2.15), collision=True, interactable=False, never_interactable=True),
+
+                Barrier_3("barrier", "Assets/exit.png", (-width//2 - 50, height-165),
+                size=(1.2, 1.3), collision=True, interactable=False, never_interactable=True),
+
+                Barrier_4("barrier", "Assets/exit.png", (-45, 250),
+                size=(.25, 1), collision=True, interactable=False, never_interactable=True),
+
+                Barrier_5("barrier", "Assets/exit.png", (335 , height - 200),
+                size=(.25, 1), collision=True, interactable=False, never_interactable=True),
+                ]   
+            Barrier_1.is_finished = True
+            Barrier_2.is_finished = True
+            Barrier_3.is_finished = True
+            Barrier_4.is_finished = True
+            Barrier_5.is_finished = True
+            self.background = pygame.image.load("level_4/sewer.png").convert_alpha()
+            self.background = pygame.transform.scale(self.background, (width*2, height*2))
+            self.show_message("I've entered the sewers, I need to find a way out", size=32, queue=True)
+            self.show_message("The sewer smell is overwhelming", size=32, queue=True)
+            self.show_message("Luckily, I have these boots that let float above the filth", size=32, queue=True)
+
+
         for item in self.items:
             item.set_message_callback(self.show_message)
+            item.level = self
+
+        self.puzzles_solved = 0
 
     def update_interactable(self, player):
         """Only the closest item within range becomes interactable"""
@@ -50,32 +158,77 @@ class Level:
                     closest_item = item
         # Set interactable only for the closest item
         for item in self.items:
-                item.glow = (item == closest_item)
-                item.can_interact_now = (item == closest_item)
-        print(closest_item)
+            item.glow = (item == closest_item)
+            item.can_interact_now = (item == closest_item)
 
     def is_finished(self):
-    # Puzzle is done if all non-door items are finished
-        puzzle_done = all(item.is_finished for item in self.items if not isinstance(item, Door))
-        if not puzzle_done:
+        """Checks if all puzzles are finished and unlocks doors if necessary"""
+        self.remaining_puzzles = sum(
+            1 for item in self.items if not isinstance(item, Door) and not item.is_finished and not item.never_interactable
+        )
+
+        # Level 1 specific logic
+        if self.level_id == 1:
+            for item in self.items:
+                if isinstance(item, Carpet) and item.is_finished:
+                    for i in self.items:
+                        if isinstance(i, Door) and i.reinteractable:
+                            i.interactable = True
+                        if isinstance(i, Picture) and i.reinteractable:
+                            i.interactable = True
+                if isinstance(item, Picture) and item.is_finished:
+                    for i in self.items:
+                        if isinstance(i, Statue_m) and i.reinteractable:
+                            i.interactable = True
+                        if isinstance(i, Statue_f) and i.reinteractable:
+                            i.interactable = True
+                        if isinstance(i, Shovel):
+                            i.interactable = True
+                        if isinstance(i, Trash) and i.reinteractable:
+                            i.interactable = True
+                        if isinstance(i, Bookshelf) and i.reinteractable:
+                            i.interactable = True
+                        if isinstance(i, MusicBox) and i.reinteractable:
+                            i.interactable = True
+        if self.level_id == 2:
+            for item in self.items:
+                if isinstance(item, Color) and item.is_finished:
+                    for i in self.items:
+                        if isinstance(i, Door) and i.reinteractable:
+                            i.interactable = True
+        if self.remaining_puzzles != 0:
+            for item in self.items:
+                if isinstance(item, Door):
+                    item.can_open = False
             return False
-        # Unlock the door(s)
+
         for item in self.items:
             if isinstance(item, Door):
-                item.interactable = True  # persistent flag, can now interact with it
-                return item.is_finished  # return True only if the door itself is finished
+                if self.level_id == 1:
+                    item.image = pygame.image.load("Assets/trapdoor_open.png").convert_alpha()
+                    item.image = pygame.transform.scale(item.image, item.resize)
+                    if not item.can_open:
+                        pygame.mixer.Sound("Assets/trap_door_open.mp3").play()
+                elif self.level_id == 2:
+                    item.image = pygame.image.load("Assets/bedroom_door_open.png").convert_alpha()
+                    item.image = pygame.transform.scale(item.image, item.resize)
+                    if not item.can_open:
+                        pygame.mixer.Sound("Assets/trap_door_open.mp3").play()
+                item.can_open = True
+                return item.is_finished
         return False
 
-    
     def draw(self, surface, player_rect):
         surface.blit(self.background, (0, 0))
-
         for item in self.items:
-            item.draw(surface, player_rect, show_hitbox=True)
+            item.draw(surface, player_rect, show_hitbox=False)
 
-    def collide_player(self, old_pos, player_rect):
+    def collide_player(self, old_pos, player_rect, dx, dy):
         for item in self.items:
-            if item.collides_with(player_rect) and item.collision==True:
-                player_rect.x = old_pos.x
-                player_rect.y = old_pos.y
-
+            if item.collision and item.collides_with(player_rect):
+                # Handle X movement separately
+                if dx != 0:
+                    player_rect.x = old_pos.x
+                # Handle Y movement separately
+                if dy != 0:
+                    player_rect.y = old_pos.y
--- main.py
+++ main.py
@@ -3,57 +3,167 @@ import pygame_menu
 import time
 from player import Player
 from level import Level
+import os
+import platform
+import ctypes
+
+if platform.system() == "Windows":
+    try:
+        ctypes.windll.user32.SetProcessDPIAware()
+    except Exception:
+        pass
+
+if platform.system() == "Darwin":
+    os.environ["SDL_HINT_VIDEO_HIGHDPI_DISABLED"] = "0"
+    os.environ["PYGAME_FORCE_HIGHDPI"] = "1"
 
 pygame.init()
 
 width, height = 1920, 1080
 center_x, center_y = width//2, height//2
-win = pygame.display.set_mode((width, height))
+win = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 player = Player(width//2, height//2)
 
-ui_font = pygame.font.Font("PressStart2P-Regular.ttf", 32)  # adjust size if needed
+ui_font = pygame.font.Font("Assets/PressStart2P-Regular.ttf", 32)  # adjust size if needed
 current_message = ""
 message_timer = 0
-def show_message(text, duration=2):
-    global current_message, message_timer
+message_queue = []
+def show_message(text, duration=2, size=32, queue=False):
+    global current_message, message_timer, ui_font, message_queue
+    if queue and current_message:
+        message_queue.append((text, duration, size))
+        return
+    ui_font = pygame.font.Font("Assets/PressStart2P-Regular.ttf", size)
     current_message = text
     message_timer = time.time() + duration
 
-
 def start():
-    menu = pygame_menu.Menu(title="Welcome", width=width, height=height, theme=pygame_menu.themes.THEME_DARK)
-    menu.add.label("Press Start to play", font_size=24)
-    menu.add.button("Start", game)
-    menu.add.button("Quit", pygame_menu.events.EXIT)
+    pygame.mixer.music.load("Assets/start_menu_music.mp3")
+    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.play(-1)
+    background_image = pygame_menu.baseimage.BaseImage("Assets/start_background.png")
+    my_theme = pygame_menu.Theme(
+        title=False,
+        background_color=background_image,
+        widget_font = pygame.font.Font("Assets/PressStart2P-Regular.ttf", 32),
+        widget_font_size = 40,
+        widget_font_color = (255, 255, 255),
+        widget_padding = 10,
+        selection_color = (255, 255, 0),
+        widget_alignment = pygame_menu.locals.ALIGN_CENTER
+    )
+    my_theme.widget_selection_effect = pygame_menu.widgets.HighlightSelection(
+        border_width=5,   # thicker border
+        margin_x=100,       # wider box
+        margin_y=70        # taller box
+    )
+    menu = pygame_menu.Menu("Welcome", width, height, theme=my_theme)
+    img = menu.add.image("Assets/start_select.png")
+    start = menu.add.button("         ", game)
+    quit  = menu.add.button("         ", pygame_menu.events.EXIT)
+    img.translate(0, 0)
+    start.translate(-9, -289.5)
+    quit.translate(-9, -215)
     menu.mainloop(win)
+
+
 def unpause():
     global paused
     paused = False
 def pause():
     global paused 
     paused = True
-    pause_menu = pygame_menu.Menu(title="Paused", width=width, height=height, theme=pygame_menu.themes.THEME_DARK)
-    pause_menu.add.label("Press Resume to continue", font_size=24) 
-    pause_menu.add.button("Resume", unpause) # Resume button: just stop the menu loop
-    pause_menu.add.button("Quit", pygame_menu.events.EXIT) # Quit button: close menu AND quit game loop
+    pygame.mixer.music.load("Assets/pause_menu_music.mp3")
+    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.play(-1)
+    screen_surface = win.copy()
+    my_theme = pygame_menu.Theme(
+        title=False,
+        background_color=(50, 50, 50, 200),   # transparentish
+        widget_alignment=pygame_menu.locals.ALIGN_CENTER,
+        widget_font=pygame.font.Font("Assets/PressStart2P-Regular.ttf", 32),
+        widget_font_size=40,
+        widget_font_color=(255, 255, 255),
+        selection_color=(255, 255, 0),
+    )
+    my_theme.widget_selection_effect = pygame_menu.widgets.HighlightSelection(
+        border_width=5,   # thicker border
+        margin_x=105,       # wider box
+        margin_y=75        # taller box
+    )
+    pause_menu = pygame_menu.Menu(title="Paused", width=width, height=height, theme=my_theme)
+    img = pause_menu.add.image("Assets/pause_select.png")
+    resume = pause_menu.add.button("         ", unpause)
+    quit  = pause_menu.add.button("         ", start)
+    img.translate(0, 0)
+    resume.translate(-9, -277.5)
+    quit.translate(-9, -190) 
     while paused:
+        win.blit(screen_surface, (0, 0))
         events = pygame.event.get()
         pause_menu.update(events)
         pause_menu.draw(win)
         pygame.display.update()
-
+    pygame.mixer.music.stop()
 def end():
-    end_menu = pygame_menu.Menu(title="Congratulations!", width=width, height=height, theme=pygame_menu.themes.THEME_DARK)
-    end_menu.add.label("You have completed the game!", font_size=32)
-    end_menu.add.button("Back to Menu", start)
-    end_menu.add.button("Quit", pygame_menu.events.EXIT)
+    frames = [
+        "Assets/end_1.png", "Assets/end_2.png", "Assets/end_3.png", "Assets/end_4.png", "Assets/end_5.png",
+        "Assets/end_6.png", "Assets/end_7.png", "Assets/end_8.png", "Assets/end_9.png", "Assets/end_10.png",
+        "Assets/end_11.png", "Assets/end_12.png", "Assets/end_13.png", "Assets/end_14.png", "Assets/end_15.png"
+    ]
+    for frame in frames:
+        img = pygame.image.load(frame).convert_alpha()
+        img = pygame.transform.scale(img,(1920, 1150))
+        win.blit(img, (0, 0))
+        pygame.display.flip()
+        if frame == frames[2]:
+            pygame.time.delay(800)
+        elif frame == frames[11] or frame == frames[12] or frame == frames[13] or frame == frames[14]:
+            time.sleep(.0001)
+        else:
+            pygame.time.delay(300)
+        if frame == frames[0]:
+            pygame.mixer.music.load("Assets/end_menu_music.mp3")
+            pygame.mixer.music.set_volume(0.5)
+            pygame.mixer.music.play(-1)
+        if frame == frames[10]:
+            pygame.time.delay(1000)   
+    pygame.time.delay(3000)
+    screen_surface = win.copy()
+    pygame.image.save(screen_surface, "Assets/temp_pause_bg.png")
+    background_image = pygame_menu.baseimage.BaseImage(image_path="Assets/temp_pause_bg.png")
+    my_theme = pygame_menu.Theme(
+        title=False,
+        background_color=background_image,
+        widget_alignment=pygame_menu.locals.ALIGN_CENTER,
+        widget_font=pygame.font.Font("Assets/PressStart2P-Regular.ttf", 32),
+        widget_font_size=40,
+        widget_font_color=(255, 255, 255),
+        selection_color=(255, 255, 0),
+    )
+    my_theme.widget_selection_effect = pygame_menu.widgets.HighlightSelection(
+        border_width=5,   # thicker border
+        margin_x=105,       # wider box
+        margin_y=75        # taller box
+    )
+    end_menu = pygame_menu.Menu("Thank you", width, height, theme=my_theme)
+    img = end_menu.add.image("Assets/end_select.png")
+    menu= end_menu.add.button("         ", start)
+    quit  = end_menu.add.button("         ", pygame_menu.events.EXIT)
+    img.translate(0, 0)
+    menu.translate(-9, -277.5)
+    quit.translate(-9, -190) 
+    end_menu.draw(win)
     end_menu.mainloop(win)
 
 
 def game():
+    global current_message, message_timer, message_queue, ui_font
+    pygame.mixer.music.stop()
     player = Player(center_x, center_y)
     level = Level(1, show_message)
+    
     while True:
         dt = clock.tick(60)
         level.update_interactable(player)
@@ -67,19 +177,22 @@ def game():
                 elif event.key == pygame.K_e:
                    player.try_interact(level.items)
         # Movement
-        old = player.handle_input(keys)
-        level.collide_player(old, player.rect)
-
+        if level.level_id == 1:
+            level_id = 1
+        elif level.level_id == 2:
+            level_id = 2
+        elif level.level_id == 3:
+            level_id = 3
+        elif level.level_id == 4:
+            level_id = 4
+        old, dx, dy = player.handle_input(keys, level_id = level_id)
+        level.collide_player(old, player.rect, dx, dy)
         # Animation
         frame = player.animate()
-
-        # Drawing
         level.draw(win, player.rect)
         win.blit(frame, (player.rect.x, player.rect.y))
-        pygame.draw.rect(win, (255, 0, 0), player.rect, 2)
         
         #next level check
-
         if level.is_finished():
             level = Level(level.level_id + 1, show_message)
             #reposition player
@@ -95,6 +208,13 @@ def game():
             pygame.draw.rect(win, (0, 0, 0), box_rect, 4)
             text_surface = ui_font.render(current_message, True, (0, 0, 0))
             win.blit(text_surface, (40, height - 90))
-
+        if current_message and time.time() >= message_timer:
+            if message_queue:
+                text, duration, size = message_queue.pop(0)
+                ui_font = pygame.font.Font("Assets/PressStart2P-Regular.ttf", size)
+                current_message = text
+                message_timer = time.time() + duration
+            else:
+                current_message = ""
         pygame.display.flip()
 start()
